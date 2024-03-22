610b04339e60721a4ba1d635c6f6fba3210bccf0
a.nejadipour <nejadipour@gmail.com>
2024-03-23 03:24:47 +0330

    Add implementation of confguration and connecting from streamlit app

---
+++ streamlit_nej_datepicker/nej_datepicker.py
@@ -0,0 +1,80 @@
+import os
+import streamlit.components.v1 as components
+from .utils import parse_config, parse_result
+from pydantic import BaseModel, ConfigDict, model_validator
+from typing import List, Union, Dict
+import datetime
+import jdatetime
+
+_RELEASE = True
+
+if _RELEASE:
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(root_dir, "frontend", "dist")
+
+    _component = components.declare_component(
+        name="nej_datepicker",
+        path=build_dir
+    )
+else:
+    _component = components.declare_component(
+        name="nej_datepicker",
+        url="http://localhost:5173"
+    )
+
+
+class Config(BaseModel):
+    """
+    The configuration of the date picker
+    """
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    always_open: bool = False
+    arrow: bool = True
+    button_type: str = 'default'
+    closed_view: str = 'text'
+    color_primary: str = '#1677ff'
+    color_primary_light: str = '#64a9f9'
+    dark_mode: bool = False
+    default_value: Union[datetime.date, jdatetime.date, List[Union[datetime.date, jdatetime.date]], Dict[
+        str, Union[datetime.date, jdatetime.date]]] = None
+    delimiter: str = '-'
+    disabled: bool = False
+    disabled_days: List[Union[datetime.date, jdatetime.date]] = []
+    locale: str = 'en'
+    maximum_date: Union[datetime.date, jdatetime.date] = None
+    maximum_today: bool = False
+    maximum_tomorrow: bool = False
+    minimum_date: Union[datetime.date, jdatetime.date] = None
+    minimum_today: bool = False
+    minimum_tomorrow: bool = False
+    placeholder: str = 'select'
+    placement: str = 'bottom'
+    return_type: str = 'datetime.date'
+    selection_mode: str = 'single'
+    selector_ending_year: int = None
+    selector_starting_year: int = None
+    should_highlight_weekends: bool = False
+    slide_animation_duration: str = '0.4s'
+    trigger: str = 'click'
+
+    @model_validator(mode='before')
+    def validate_default_value(cls, data: dict):
+        locale = data.get("locale", "en")
+        keys = data.keys()
+        if "placeholder" not in keys:
+            data["placeholder"] = 'select' if locale == 'en' else 'انتخاب'
+        if "return_type" not in keys:
+            data["return_type"] = 'datetime.date' if locale == 'en' else 'jdatetime.date'
+
+        return data
+
+
+class NejDatepicker:
+    def __call__(self, *args, config: Config, **kwargs):
+        return_type = config.return_type
+        config = parse_config(config.dict())
+        return parse_result(result=_component(config=config, args=args, **kwargs), return_type=return_type)
+
+
+datepicker_component = NejDatepicker()
---
+++ streamlit_nej_datepicker/utils.py
@@ -0,0 +1,148 @@
+import datetime
+from typing import Union
+import jdatetime
+
+DATE_KEYS = ["maximum_date", "minimum_date"]
+CLOSED_VIEW_KEYS = [("button_type", "type")]
+IGNORE_KEYS = ["return_type"]
+
+
+def parse_config(config: dict) -> dict:
+    """
+    Parse the config dictionary in order to match the props of React component
+
+    Parameters
+    ----------
+    config: dict
+        The configuration dictionary
+
+    Returns
+    -------
+    dict
+        The parsed configuration dictionary
+    """
+    # remove the keys that are not needed in the React component
+    config = {key: value for key, value in config.items() if key not in IGNORE_KEYS}
+
+    # parse all the date related keys to convert them into dict
+    locale = config.get("locale", "en")
+    for key in DATE_KEYS:
+        if key in config:
+            config[key] = parse_date_based_on_locale(config[key], locale)
+    if "default_value" in config:
+        default_value = config["default_value"]
+        if isinstance(default_value, (datetime.date, jdatetime.date)):
+            config["default_value"] = parse_date_based_on_locale(default_value, locale)
+        elif isinstance(default_value, list):
+            config["default_value"] = [parse_date_based_on_locale(date, locale) for date in default_value]
+        elif isinstance(default_value, dict):
+            config["default_value"] = {
+                key: parse_date_based_on_locale(value, locale) for key, value in default_value.items()
+            }
+    if "disabled_days" in config:
+        disabled_days = config["disabled_days"]
+        config["disabled_days"] = [parse_date_based_on_locale(date, locale) for date in disabled_days]
+
+    # group all the closed view related keys into single key
+    config["closed_view_props"] = {}
+    for old_key, new_key in CLOSED_VIEW_KEYS:
+        if old_key in config:
+            config["closed_view_props"][new_key] = config[old_key]
+            del config[old_key]
+
+    # change all the keys from snake_case to camelCase
+    config = {convert_to_camel_case(key): value for key, value in config.items()}
+
+    return config
+
+
+def convert_to_camel_case(snake_str: str) -> str:
+    """
+    Convert the snake_case string to camelCase
+
+    Parameters
+    ----------
+    snake_str: str
+        The snake_case string
+
+    Returns
+    -------
+    str
+        The camelCase string
+    """
+    first, *others = snake_str.split('_')
+    return ''.join([first, *map(str.title, others)])
+
+
+def parse_date_based_on_locale(date: Union[datetime.date, jdatetime.date], locale: str = "en") -> dict:
+    """
+    Parse the date based on the locale
+
+    Parameters
+    ----------
+    date: Union[datetime.date, jdatetime.date]
+        The date object
+    locale: str
+        The locale
+
+    Returns
+    -------
+    dict
+        The parsed date dictionary
+    """
+    if locale == "en":
+        if isinstance(date, datetime.date):
+            return dict(
+                year=date.year,
+                month=date.month,
+                day=date.day
+            )
+        elif isinstance(date, jdatetime.date):
+            en_date = date.togregorian()
+            return dict(
+                year=en_date.year,
+                month=en_date.month,
+                day=en_date.day
+            )
+    elif locale == "fa":
+        if isinstance(date, datetime.date):
+            j_date = jdatetime.date.fromgregorian(date=date)
+            return dict(
+                year=j_date.year,
+                month=j_date.month,
+                day=j_date.day
+            )
+        elif isinstance(date, jdatetime.date):
+            return dict(
+                year=date.year,
+                month=date.month,
+                day=date.day
+            )
+    else:
+        raise ValueError("Invalid locale")
+
+
+def parse_result(result: dict, return_type: str = "datetime.date") -> None | datetime.date | jdatetime.date | None:
+    """
+    Parse the result based on the return_type
+
+    Parameters
+    ----------
+    result: dict
+        The result dictionary from the React component
+    return_type: str
+        The return type
+
+    Returns
+    -------
+    Union[datetime.date, jdatetime.date]
+        The parsed result
+    """
+    if result is None:
+        return None
+
+    year, month, day = result["year"], result["month"], result["day"]
+    date = datetime.date(year=year, month=month, day=day) if return_type == "datetime.date" else jdatetime.date(
+        year=year, month=month, day=day)
+
+    return date
